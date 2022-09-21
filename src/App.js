d6d0819b2add295686e8f9a56ccfce4375b894d5
kanishweb <timespartner9941@gmail.com>
2022-09-21 19:29:32 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -6,17 +6,25 @@ import ErrorBoundary from './compound/ErrorBoundary';
 import Error from "./screens/Error";
  import Login from "./screens/login";
 import FAQs from "./screens/faq";
+import View from "./screens/view";
+import Home from "./screens/home";
+import Add from "./compound/add";
+import Addlist from "./compound/addlist";
 
 function App() {
   return (
     <div className="App">
-      <ErrorBoundary>
-
+    
+<ErrorBoundary>
       <BrowserRouter>
       <Routes>
           <Route path="/" element={<Landingpage/>}/> 
           <Route path="/login"  index element={<Login/>} />
           <Route path="/faqs"   element={<FAQs/>} /> 
+          <Route path="/home"   element={<Home/>} /> 
+          <Route path="/add"   element={<Add/>} /> 
+          <Route path="/view"   element={<View/>} /> 
+          <Route path="/addlist"   element={<Addlist/>} /> 
           <Route path="*" element={<Error/>} />
 
           
