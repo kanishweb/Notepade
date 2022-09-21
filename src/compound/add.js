e5dfb84885f897cfb050ca94502e8e0c149c5e20
kanishweb <110451306+kanishweb@users.noreply.github.com>
2022-09-21 18:56:59 +0530

    Add files via upload

---
+++ src/compound/add.js
@@ -0,0 +1,97 @@
+
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Navbar from "../compound/MenuAppBar";
+import Swal from 'sweetalert2'
+import Footer from "../compound/footer";
+
+
+function Add() {
+  const [title, setTitle] = useState();
+  const [description, setDescription] = useState();
+
+  const Swal = require('sweetalert2')
+  const navigate = useNavigate();
+
+  const [listed, setListed] = useState([]);
+
+  const inputchange = (e) => {
+    setTitle(e.target.value)
+  }
+
+  const inputmail = (e) => {
+    setDescription(e.target.value)
+  }
+
+  const addclick = () => {
+   
+    
+    if (title === "") {
+      Swal.fire(
+        'The Internet?',
+        'Enter your Title',
+        'question'
+      )
+    
+    }
+    else if (description === "") {
+      Swal.fire(
+        'The Internet?',
+        'Enter your Description',
+        'question'
+      )
+     
+
+    }
+    else {
+
+      setListed((oldData) => {
+        let tempre = [...oldData, { 'id': listed.length + 1, 'title': title, 'description': description }]
+        
+        return (tempre)
+        
+      })
+      Swal.fire(
+        'Good job!',
+        'Your Entry is Success!',
+        'success'
+      )
+      // navigate('/addlist',{state:listed}) 
+    }
+
+    setTitle('');
+    setDescription('');
+
+
+  }
+  const add= (props) => {
+    addclick(props)
+    navigate('/home',{state:listed})
+  }
+
+
+
+ 
+
+  return (
+    <div>
+    <Navbar />
+    <div className='slide1'>
+      <h3 className='h3tx'> CLG School Notepade</h3>
+      <div>
+        <input className='textview2' name='title' type='text' value={title} onChange={(e) => inputchange(e)} placeholder='Enter your Titel' required/><br /><br />
+        <textarea   className='textview2' name='description' type='text' value={description} onChange={(e) => inputmail(e)} placeholder='Enter your Description' required data-editor-type="wmd"  cols="42" rows="5" tabindex="101" data-min-length=""></textarea><br /><br />
+        <button className='exbtn' onClick={()=> add()} >Add</button>
+        {/* <button className='exbtn'  onClick={() => { navigate('/home',{state:listed}) }}>View</button> */}
+      </div>
+
+   
+
+    </div>
+    <Footer/>
+    </div>
+  );
+
+}
+
+export default Add;
---
+++ src/compound/addlist.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import {useNavigate,useLocation } from "react-router-dom";
+import {Button } from "@mui/material";
+import ModeEdit from '@mui/icons-material/ModeEdit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import {useState} from 'react'; 
+import {Table,TableContainer,TableHead,TableRow,TableCell,TableBody,Paper} from '@mui/material';
+// import Add from "./compound/add";
+
+
+
+
+function Addlist() {
+
+
+  const navigate=useNavigate();
+  const {state} = useLocation();
+  const[task1,setTask1]=useState(state);
+      
+  // const [list] =useState([state])
+
+
+  const deleteclick=(index)=>{
+
+    task1.splice(index, 1);
+    setTask1([...task1]);
+
+    alert('delete');
+
+  }
+
+  const View=(item)=>{
+    navigate('/view',{state:item});
+  }
+  const handleEdit=()=>{
+    // setName(allData[i])
+    // setEditIndex(i)
+     navigate('/add');
+}
+
+
+
+  return (
+    <div>
+    <div className='plus'>
+<AddCircleIcon onClick={()=>{navigate('/add')}}></AddCircleIcon>
+<span style={{color: '#918f8d',display:'block'}}>Add Your Note</span>
+</div>
+ <div className='listed'>
+          
+          
+          <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 50 }} aria-label="simple table">
+                  <TableHead>
+                    <TableRow style={{backgroundColor:'#cbade6'}}>
+                      <TableCell style={{color:'#000',width:150,fontWeight:'bold'}}>Title</TableCell>
+                      <TableCell style={{color:'#000',fontWeight:'bold'}}>Delete</TableCell>
+                      <TableCell style={{color:'#000',fontWeight:'bold'}}>Edit</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                   
+                  
+                    { task1?.length > 0 && task1.map((item,index) => {
+                      return (
+                      
+                        <TableRow
+                            key={item.title}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                          >
+                          
+                            <TableCell onClick={() => View(item)} component="th" scope="row">
+                              {item.title}
+                            </TableCell>
+                           
+                            <TableCell  component="th" scope="row">
+                            <Button className='delete' variant="outlined" onClick={()=>deleteclick(index)} startIcon={<DeleteIcon />}>
+                                Delete
+                            </Button>
+                            </TableCell>
+                           
+                            <TableCell component="th" scope="row">
+                            <Button className='edit' variant="outlined"  startIcon={<ModeEdit />} onClick={()=>handleEdit()}  >
+                                Edit
+                            </Button>
+                            </TableCell>
+                            
+                        </TableRow>
+                  
+                    
+                      );
+                        })}
+                
+                  </TableBody>
+                </Table>
+              </TableContainer>
+                    </div>
+
+    </div>
+  )
+}
+
+export default Addlist;
